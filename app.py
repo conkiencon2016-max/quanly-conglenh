00e1f6f34bedb0a4a43c449a93368d656355d2db
conkiencon2016-max <conkiencon2016@gmail.com>
2026-03-12 21:41:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1205,7 +1205,10 @@ if os.environ.get("RUN_MAIN") != "true":
     scheduler.start()
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 10000))
+
+    app.run(host="0.0.0.0", port=port, debug=False)
+
 
 
 
