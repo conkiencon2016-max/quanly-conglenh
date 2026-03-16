47b2e70d835b54553b70f3fca7c5032bb772668b
conkiencon2016-max <conkiencon2016@gmail.com>
2026-03-16 15:17:14 +0700

    Update backup_drive.py

--- backup_drive.py
+++ backup_drive.py
@@ -1,12 +1,12 @@
 import os
 import json
+from datetime import datetime
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import MediaFileUpload
 
 DB_FILE = "conglenh.db"
 
-# đọc credentials từ ENV
 creds_json = os.environ["GOOGLE_CREDENTIALS_JSON"]
 creds_dict = json.loads(creds_json)
 
@@ -17,16 +17,18 @@ creds = service_account.Credentials.from_service_account_info(
 
 service = build("drive", "v3", credentials=creds)
 
+today = datetime.now().strftime("%Y%m%d_%H%M")
+
 file_metadata = {
-    "name": f"conglenh_backup_{os.path.basename(DB_FILE)}"
+"name": f"conglenh_backup_{today}.db"
 }
 
 media = MediaFileUpload(DB_FILE, mimetype="application/octet-stream")
 
 file = service.files().create(
-    body=file_metadata,
-    media_body=media,
-    fields="id"
+body=file_metadata,
+media_body=media,
+fields="id"
 ).execute()
 
-print("Upload success. File ID:", file.get("id"))
+print("Backup Google Drive thành công:", file.get("id"))
